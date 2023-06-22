3f511a4f7d2ef6670828214bacfe655a331a099e
Filip <filip.tokar@seznam.cz>
2023-06-22 18:37:44 +0200

    rest of nextAuth and database link

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,28 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
-    description: 'Discover & Share AI Prompts'
-    
-}
-const layout = () => {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+const Rootlayout = ({ children }) => {
   return (
-    <html lang='en'>
-        <body>
-            <div className='main'>
-                <div className='gradient'/>
-            </div>
-        </body>
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
-export default layout
+export default Rootlayout;
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,20 @@
-
+import Feed from "@components/Feed";
 
 const Home = () => {
   return (
-    <div>
-      page
-    </div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">AI-powered prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is collection of AI prompts. It provides you a place to
+        discover, create and share interesting prompts
+      </p>
+      <Feed/>
+    </section>
+  );
+};
 
-export default page
+export default Home;
